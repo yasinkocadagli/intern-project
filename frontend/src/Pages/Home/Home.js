aa3af89772990ee255c120623e90904307cf1937
yasin <yasinkocadagli@gmail.com>
2023-08-29 11:47:50 +0300

    tag name updated

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -9,15 +9,15 @@ const Home = () => {
 
     
       <main>
-        <div>
+        <nav>
           <div>
             <span class="material-symbols-outlined">notes</span>
             <button>Son güncelleme</button>
           </div>
           <span class="material-symbols-outlined">search</span>
           <input type="search" placeholder="Tablolarımı ara" />
-        </div>
-        <div>
+        </nav>
+        <content>
           <div>
             {/*<img src={tablesImg} alt="company photos" />*/}
             <span class="material-symbols-outlined">star</span>
@@ -26,7 +26,7 @@ const Home = () => {
             <Link to="/table">Company tables</Link>
 
           </div>
-        </div>
+        </content>
 
       </main>
     </Fragment>
