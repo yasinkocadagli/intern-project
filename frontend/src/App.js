9ea44c7e338a62ed8c0d00284c01fd7dd128760b
yasin <yasinkocadagli@gmail.com>
2023-09-06 13:57:10 +0300

    Login page rework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Table from "./Pages/Tables/Table";
 import Layout from "./components/Layout/Layout";
-import Login from "./Pages/Login/Login";
+
 
 function App() {
   return (
     <Fragment>
-      <Login />
-
-      {/* <Routes>
-        <Route path="/" element={<Layout/>}/>
-        <Route path="/table" element={<Table/>}/>
-      </Routes> */}
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/table" element={<Table />} />
+      </Routes>
     </Fragment>
   );
 }
